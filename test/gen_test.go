2ea2a9e72c9b62e27bc914a53fe259ee98094829
maxilozoz <2802146520@qq.com>
2020-12-26 23:38:54 +0800

    feat: auto set edges and skip auto option

--- test/gen_test.go
+++ test/gen_test.go
@@ -2,9 +2,12 @@ package test
 
 import (
 	"bytes"
+	"github.com/casbin/casbin/v2"
+	"github.com/casbin/casbin/v2/model"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	_ "golang.org/x/tools/go/packages"
+	"log"
 	"os"
 	"os/exec"
 	"testing"
@@ -24,4 +27,36 @@ func TestGen(t *testing.T) {
 func TestMapArray(t *testing.T) {
 	x := make(map[string][]string, 0)
 	assert.Len(t, x["?"], 0)
+}
+
+func TestCasbin(t *testing.T) {
+	m, err := model.NewModelFromString(`
+[request_definition]
+r = sub, obj, act
+
+[policy_definition]
+p = sub, obj, act
+
+[policy_effect]
+e = some(where (p.eft == allow))
+
+[matchers]
+m = r.sub == p.sub && r.obj == p.obj && r.act == p.act
+`)
+	if err != nil {
+		log.Fatalf("error: model: %s", err)
+	}
+
+	e, err := casbin.NewEnforcer(m)
+	if err != nil {
+		log.Fatalf("error: enforcer: %s", err)
+	}
+	rule := []string{"role1", "obj", "act"}
+	e.AddPolicy(rule)
+	//that's ok
+	rule[0] = "role2"
+	//adapter data ok
+	//e.model["p"]["p"].Policy is invaild, role1,obj,act not exist role2,obj,act exist
+	pass, _ := e.Enforce("role1", "obj", "act")
+	require.Equal(t, pass, false)
 }
